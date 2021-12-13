dcdac5fbcbf59c15df0779f005732a1e1ef0b472
rayanepimentel <radharayg@gmail.com>
2021-12-13 16:40:45 -0300

    sem erro

--- 01/index.js
+++ 01/index.js
@@ -7,13 +7,13 @@
 //}
 
 // sem strict 
-asim = 1;
+let asim = 1;
 console.log(window.asim);
 
 let theval = 0;
 
-thval = 1
-    //theval = 1 
+//thval = 1
+theval = 1
 
 if (theval > 0) {
     console.log("hihiihih");
