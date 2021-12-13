d0f5501b0f1438355c899517086f94294546499f
rayanepimentel <radharayg@gmail.com>
2021-12-13 16:37:22 -0300

    simulando erro sem "use strict"

---
+++ 01/index.js
@@ -0,0 +1,19 @@
+//"use strict"
+
+//not strict moode.. --> legado
+//function newCode() {
+//  "use strict";
+//strict moode...
+//}
+
+// sem strict 
+asim = 1;
+console.log(window.asim);
+
+var theval = 0;
+
+thval = 1
+
+if (theval > 0) {
+    console.log("hihiihih");
+}
