83e223f9bcda5c7d56e83008da4b82cabf33174d
rayanepimentel <radharayg@gmail.com>
2021-12-14 09:10:20 -0300

    rest op

--- 01/index.js
+++ 01/index.js
@@ -70,6 +70,75 @@ function foo2(c, d) {
 foo2(c, d);
 console.log(c + JSON.stringify(d)); // 1 {'moo':3}
 
-var asimsVar = 3;
-asimVar = 1;
-console.log(asimVar);
+//var asimsVar = 3;
+//asimVar = 1;
+//console.log(asimVar);
+
+
+function sum(a, b) {
+    return a + b;
+}
+
+sum(1, 2); //3
+console.log(sum(1, 2, 3, 4, 5)); // 3 ignora os d+ params
+console.log(sum(1)); //NaN, houve algum problem no calc
+//todos os arguments sao passado para função com uma var ou propriedade = os argumentos
+//
+function sum1(a, b) {
+    console.log(arguments);
+    return a + b;
+}
+
+sum1(2, 6, 8, 12); //array Arguments { 0: 2, 1: 6, 2: 8, 3: 12, callee: f sum1(a,b), length:4}
+
+
+//exemplo não indicado
+//- function nao indica quantos arg (var/propriedade) sao necessarios
+//- vc realmente precisa olhar p/ code para entender o que tá acontecendo
+//- misturar arg fixos com var 
+//array de arg nao é certo
+function sum2() {
+    var total = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+    }
+
+    return total;
+}
+
+console.log(sum2(1, 3, 5, 7, 9)) //25
+
+//bom exemplo
+
+function login(method) {
+    //var options = arguments.slice(1); //erro, arg nao é uma array
+    var options = Array.prototype.slice(arguments, 1); //basicamente arg torna array
+    console.log(method);
+    console.log(options);
+}
+
+login('msn', 1, 2, 3, 4, 5);
+//login('twitter', 5, 8, 9)
+
+function login1(...options) {
+    //pode add elementos com push
+    options.push(11);
+    console.log(options);
+}
+
+login1('msn1', 0, 3, 6, 9);
+
+
+function login2(mth, ...options) {
+    //pode add elementos com push
+    console.log(mth);
+    options.push(11);
+    console.log(options);
+}
+
+login2('msn2', 0, 3, 6, 9);
+//o ...rest tem sempre que ficar por 'último'
+//nao podemos fazer isso:
+/*
+function(mth, ...options, moo)
+*/
